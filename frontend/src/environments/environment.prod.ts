9b1c5d959383bbce02b7f569764bce0bf5ceb713
Alex Koziy <alex.ksoft@gmail.com>
2026-03-26 14:27:23 +0200

    deploy on AWS Lambda

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'   // Nginx proxies /api → backend:8080/api
+  apiUrl: 'https://kwf71ynxs0.execute-api.us-east-1.amazonaws.com/prod/api'
 };
